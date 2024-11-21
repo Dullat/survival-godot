bb0119f2fcf3188bcf2ef499f0c46552b04577f6
dullat <dullat@users.noreply.github.com>
2024-11-21 20:07:28 +0530

    apple falling scripts and animation

---
+++ SCRIPTS/apple_collectable.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+var motion = Vector2()
+
+func _ready() -> void:
+	$AnimationPlayer.play("fade")
+	print("started")
+
+func _physics_process(delta):
+	var player = get_parent().get_node("player")
+	position += (player.position - position)/10
+	look_at(player.position)
+	move_and_collide(motion)
+	await get_tree().create_timer(1).timeout
+	$AnimationPlayer.stop()
+	_pop_apple()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		_pop_apple()
+		
+
+func _pop_apple():
+	queue_free()
